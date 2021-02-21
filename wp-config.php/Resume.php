d6b8f9ed6fe2d6715bb23f18b3454203bc0c9a20
Alcaudon89 <19646521+alcaudon89@users.noreply.github.com>
2021-02-21 20:15:55 +0100

    Updated note 'Resume'

---
+++ wp-config.php/Resume.php
@@ -0,0 +1,13 @@
+// Configuracion DavidRD.es
+define( 'DISABLE_WP_CRON', true);
+define( 'WP_MEMORY_LIMIT', '256M' );
+define( 'WP_MAX_MEMORY_LIMIT', '256M' );
+define( 'DISALLOW_FILE_EDIT', true );
+define( 'FORCE_SSL_ADMIN', true );
+define( 'FORCE_SSL_LOGIN', true );
+define( 'CONCATENATE_SCRIPTS', true);
+define( 'COMPRESS_CSS', true);
+define( 'COMPRESS_SCRIPTS', true);
+define( 'WP_SITEURL', 'https://miweb.com');
+define( 'WP_HOME', 'https://miweb.com');
+define( 'IMAGE_EDIT_OVERWRITE', true);
